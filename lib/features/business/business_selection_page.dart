e055e945381c7d8ac09ab3c57f691f49dc4ec8f7
loumy96-debug <loumyedson880@gmail.com>
2025-12-19 18:16:25 +0300

    Add business selection page UI

--- lib/features/business/business_selection_page.dart
+++ lib/features/business/business_selection_page.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
 
+class BusinessSelectionPage extends StatelessWidget {
+  const BusinessSelectionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final businesses = [
+      'Shop A',
+      'Shop B',
+      'Shop C',
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Select Business'),
+      ),
+      body: ListView.builder(
+        itemCount: businesses.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(businesses[index]),
+            trailing: const Icon(Icons.arrow_forward),
+            onTap: () {
+              // Next step later
+            },
+          );
+        },
+      ),
+    );
+  }
+}
